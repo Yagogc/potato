1adbe6808915bd9406a43d8447ade6e434bee72c
Yago <jagogc@gmail.com>
2018-04-14 20:11:19 +0100

    Added Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from "react";
+
+import fetchJsonp from "fetch-jsonp";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 class App extends Component {
+  state = {
+    items: ""
+  };
+  componentDidMount() {
+    let items;
+    fetchJsonp(
+      "https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json",
+      {
+        jsonpCallbackFunction: "jsonFlickrFeed"
+      }
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        console.log("parsed json", json.items);
+        items = json.items;
+      })
+      .catch(function(ex) {
+        console.log("parsing failed", ex);
+      });
+    this.setState({
+      items: items
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
         <Header />
-        <main>main</main>
+        <Home items="{this.state.items}" />
       </React.Fragment>
     );
   }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Container from "./../ui/Container";
+
+class Home extends Component {
+  render() {
+    return (
+      <main>
+        <Container>Home</Container>
+      </main>
+    );
+  }
+}
+
+export default Home;
